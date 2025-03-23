a6504845fcb99f64a7bebc5f4e2fe4bb0f11aa92
Uchana <uchaneishvili01@gmail.com>
2025-03-24 00:28:50 +0400

    first commit

--- src/Second/ArraySearcher.java
+++ src/Second/ArraySearcher.java
@@ -1,14 +1,13 @@
 package Second;
 
 public class ArraySearcher {
-    // მეთოდი, რომელიც ეძებს რიცხვს მასივში და ბეჭდავს მის ინდექსს
     public static void findNumber(int[] array, int number) {
-        boolean found = false; // მონიშნავს, თუ ვიპოვეთ რიცხვი
+        boolean found = false;
         for (int i = 0; i < array.length; i++) {
             if (array[i] == number) {
                 System.out.println("ნაპოვნია ინდექსზე: " + i);
                 found = true;
-                break; // ვწყვეტთ ციკლს, რადგან რიცხვი უკვე ვიპოვეთ
+                break;
             }
         }
         if (!found) {
